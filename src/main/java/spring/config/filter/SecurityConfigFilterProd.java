f156a626d74d85511cd029adb3840ac9ccd62c5b
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-09-05 12:15:50 -0300

    updated resquest filter name

--- src/main/java/spring/config/filter/SecurityConfigFilterProd.java
+++ src/main/java/spring/config/filter/SecurityConfigFilterProd.java
@@ -1,46 +1,35 @@
-package spring.config.security;
+package spring.config.filter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import spring.config.security.provider.CustomProviderAuthentication;
 import spring.config.security.exceptionHandling.CustomBasicAuthenticationEntryPoint;
 import spring.config.security.exceptionHandling.CustomHandlerAccessDeniedHandler;
-import spring.config.filter.JWT.JWTTokenGeneratorFilter;
-import spring.config.filter.JWT.JWTValidatorFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@Profile("!prod")
-public class SecurityConfigFilter {
+@Profile("prod")
+public class SecurityConfigFilterProd {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,JWTTokenGeneratorFilter jwtTokenGeneratorFilter, JWTValidatorFilter jwtValidatorFilter) throws Exception {
-        http.sessionManagement((smc-> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement((smc-> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS).maximumSessions(5).maxSessionsPreventsLogin(true)))
                 .requiresChannel(rcc-> rcc.anyRequest().requiresInsecure())
                 .csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/user/register").authenticated()
-                .requestMatchers("/info/**").permitAll()
-                .requestMatchers("/user/apiLogin/**").permitAll()
                 .anyRequest().authenticated());
         http.formLogin(withDefaults());
         http.httpBasic(hbs -> hbs.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(ehc-> ehc.accessDeniedHandler(new CustomHandlerAccessDeniedHandler()));
-        http.addFilterAfter(jwtTokenGeneratorFilter, BasicAuthenticationFilter.class);
-        http.addFilterBefore(jwtValidatorFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
@@ -52,13 +41,5 @@ public class SecurityConfigFilter {
     @Bean
     public CompromisedPasswordChecker compromisedPasswordChecker(){
         return new HaveIBeenPwnedRestApiPasswordChecker();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        CustomProviderAuthentication customAuthentication = new CustomProviderAuthentication(userDetailsService, passwordEncoder);
-        ProviderManager providerManager = new ProviderManager(customAuthentication);
-        providerManager.setEraseCredentialsAfterAuthentication(false);
-        return providerManager;
     }
 }
